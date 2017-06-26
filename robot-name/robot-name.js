9c64704bb9f17c36e339d93d5a7b17253a429725
omaotzu <omarhphillips@gmail.com>
2017-06-26 16:51:48 +0100

    unable to get the last piece working no idea why

---
+++ robot-name/robot-name.js
@@ -0,0 +1,38 @@
+var Robot = function (){
+  this.used = [];
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+  function randomLetters(){
+    return `${alphabet[Math.floor((Math.random() * 26))]}${alphabet[Math.floor((Math.random() * 26))]}`;
+  }
+  function randomNumbers(){
+    return `${Math.floor((Math.random() * 10))}${Math.floor((Math.random() * 10))}${Math.floor((Math.random() * 10))}`;
+  }
+
+  this.compileName = function() {
+    return `${randomLetters()}${randomNumbers()}`;
+  };
+
+  this.createName = function(){
+    let name = this.compileName();
+
+    while(this.used.includes(name)){
+      name = this.compileName();
+    }
+    this.used.push(name);
+
+    return name;
+  };
+
+  this.name = this.createName();
+
+
+  this.reset = function () {
+    this.name = this.createName();
+  };
+};
+
+
+
+module.exports = Robot;
